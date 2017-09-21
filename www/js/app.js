6323dc292b96f555099f25c5f349c80811b7cc0d
Richard Mascarini <richardwmascarini@gmail.com>
2017-09-20 22:15:53 -0300

    Login/Lista de produtos e cadastro de usuario

--- www/js/app.js
+++ www/js/app.js
@@ -1,24 +1,108 @@
-// Ionic Starter App
+var app = angular.module('servfesta', ['ionic']);
 
-// angular.module is a global place for creating, registering and retrieving Angular modules
-// 'starter' is the name of this angular module example (also set in a <body> attribute in index.html)
-// the 2nd parameter is an array of 'requires'
-angular.module('starter', ['ionic'])
-
-.run(function($ionicPlatform) {
-  $ionicPlatform.ready(function() {
-    if(window.cordova && window.cordova.plugins.Keyboard) {
-      // Hide the accessory bar by default (remove this to show the accessory bar above the keyboard
-      // for form inputs)
+app.run(function ($ionicPlatform) {
+  $ionicPlatform.ready(function () {
+    if (window.cordova && window.cordova.plugins.Keyboard) {
       cordova.plugins.Keyboard.hideKeyboardAccessoryBar(true);
-
-      // Don't remove this line unless you know what you are doing. It stops the viewport
-      // from snapping when text inputs are focused. Ionic handles this internally for
-      // a much nicer keyboard experience.
       cordova.plugins.Keyboard.disableScroll(true);
     }
-    if(window.StatusBar) {
+    if (window.StatusBar) {
       StatusBar.styleDefault();
     }
   });
-})
+});
+
+app.config(function ($stateProvider, $urlRouterProvider) {
+  $stateProvider.state('login', {
+    url: '/login',
+    templateUrl: 'templates/login.html',
+    controller: 'LoginCtrl'
+  });
+
+  $stateProvider.state('cadastroUsuario', {
+    url: '/novo-usuario',
+    templateUrl: 'templates/cadastro.html',
+    controller: 'CadastroUsuarioCtrl'
+  });
+
+  $stateProvider.state('produtos', {
+    url: '/produtos',
+    templateUrl: 'templates/produtos.html',
+    controller: 'ProdutosCtrl'
+  });
+
+  $urlRouterProvider.otherwise('/login');
+});
+
+app.controller("LoginCtrl", function ($scope, $state) {
+  $scope.form = {
+    email: '',
+    senha: ''
+  };
+
+  $scope.form.mensagens = [];
+
+  $scope.cadastroUsuario = function () {
+    $state.go('cadastroUsuario');
+  };
+
+  $scope.login = function () {
+    if (this.form.email === '' || this.form.senha === '') {
+      return this.form.mensagens.push("Preencha os campos");
+    }
+
+    $state.go('produtos');
+  }
+});
+
+app.controller('CadastroUsuarioCtrl', function ($scope, $state) {
+  $scope.form = {
+    email:'',
+    senha: '',
+    confirmarSenha: ''
+  };
+
+  $scope.form.mensagens = [];
+
+  $scope.novoCadastro = function () {
+    $state.go('produtos')
+  };
+});
+
+app.controller('ProdutosCtrl', function ($scope, ProdutoService, $state) {
+  var produto1 = {
+    nome:'Refrigerantes',
+    preco:'R$ 5,89'
+  };
+
+  //insere na lista
+  ProdutoService.create(produto1);
+
+  //exibir os produtos.
+  $scope.produtos = ProdutoService.read();
+
+  $scope.adicionarNovoProduto = function () {
+    //chamar tela de cadastro
+  };
+
+  $scope.editarProduto = function () {
+
+  };
+
+});
+
+app.factory('ProdutoService', function () {
+  var lista = []; // banco volatil
+
+  //exibe a lista
+  return{
+    read: function () {
+ return lista;
+    },
+
+    //recebe parametros para adicionar no banco volatil
+    create:function (objetoProduto) {
+      lista.push(objetoProduto)
+    }
+  }
+});
