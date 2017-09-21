7b64874ca5753d03ce774bd1c79421554ebe18a9
Richard Mascarini <richardwmascarini@gmail.com>
2017-09-21 18:09:43 -0300

    Editar produto ok

--- www/js/app.js
+++ www/js/app.js
@@ -27,93 +27,21 @@ app.config(function ($stateProvider, $urlRouterProvider) {
 
   $stateProvider.state('produtos', {
     url: '/produtos',
-    templateUrl: 'templates/product/produto.html',
-    controller: 'ProdutosCtrl'
-  });
+    templateUrl: 'templates/product/listar.html',
+    controller: 'ProdutosListarCtrl'
 
+  });
   $stateProvider.state('novoProduto', {
     url: '/novo-produto',
     templateUrl: 'templates/product/cadastro.html',
-    controller: 'ProdutosCtrl'
+    controller: 'ProdutosCadastrarCtrl'
   });
 
   $stateProvider.state('editarProduto', {
     url: '/editar-produto/:produto',
-    templateUrl: 'templates/product/cadastro.html',
-    controller: 'ProdutosCtrl',
-    resolve: {
-      produto: function ($stateParams, ProdutoService) {
-        const produtos = ProdutoService.read()
-        for (i = 0; i < produtos.length; i++) {
-          if (produtos[i].nome == $stateParams.produto){
-            return produtos[i]
-          }
-        }
-
-        return $state.go('produtos');
-      }
-    }
+    templateUrl: 'templates/product/editar.html',
+    controller: 'ProdutosEditarCtrl'
   });
 
   $urlRouterProvider.otherwise('/login');
-});
-
-app.controller('CadastroUsuarioCtrl', function ($scope, $state) {
-  $scope.form = {
-    email: '',
-    senha: '',
-    confirmarSenha: ''
-  };
-
-  $scope.form.mensagens = [];
-
-  $scope.novoCadastro = function () {
-    $state.go('produtos')
-  };
-});
-
-app.controller('ProdutosCtrl', function ($scope, ProdutoService, $state) {
-  //exibir os produtos.
-  $scope.produtos = ProdutoService.read();
-
-  for (i = 0; i < $scope.produtos.length; i++) {
-    if ($scope.produtos[i].nome == $state.produto){
-      $scope.produto = produtos[i]
-    }
-  }
-
-  var produto1 = {
-    nome: 'Refrigerantes',
-    preco: 'R$ 5,89'
-  };
-
-  //insere na lista
-  ProdutoService.create(produto1);
-
-  $scope.adicionarNovoProduto = function () {
-    //chamar tela de cadastro de um novo produto
-    $state.go('novoProduto');
-  };
-
-  $scope.editarProduto = function (produto) {
-    console.log(produto)
-    $state.go('editarProduto', { produto: produto.nome });
-  };
-
-});
-
-app.factory('ProdutoService', function () {
-  var lista = []; // banco volatil
-
-  //exibe a lista
-  return {
-    read: function () {
-      return lista;
-    },
-
-    //recebe parametros para adicionar no banco volatil
-    create: function (objetoProduto) {
-      lista.push(objetoProduto)
-    }
-  }
 });
---
+++ www/js/controller/user/cadastrar.js
@@ -0,0 +1,13 @@
+app.controller('CadastroUsuarioCtrl', function ($scope, $state) {
+  $scope.form = {
+    email: '',
+    senha: '',
+    confirmarSenha: ''
+  };
+
+  $scope.form.mensagens = [];
+
+  $scope.novoCadastro = function () {
+    $state.go('produtos')
+  };
+});
